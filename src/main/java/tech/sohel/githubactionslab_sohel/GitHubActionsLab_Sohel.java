3e859ae0386878ecc53ac214f80fc644f7f7eb6b
Sohel Shekh <contact.sohelshekh@gmail.com>
2026-06-10 13:48:28 -0400

    Init'

---
+++ src/main/java/tech/sohel/githubactionslab_sohel/GitHubActionsLab_Sohel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tech.sohel.githubactionslab_sohel;
+
+/**
+ *
+ * @author Sohel Shekh, 2026
+ */
+public class GitHubActionsLab_Sohel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from Github Actions lab!");
+    }
+}
